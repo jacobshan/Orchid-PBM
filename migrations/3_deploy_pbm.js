5a000db4f11495b73e43cf806512be09778249fd
Mihir <mihirmohan38@gmail.com>
2022-09-22 14:32:29 +0800

    chore: updating migrating

--- migrations/3_deploy_pbm.js
+++ migrations/3_deploy_pbm.js
@@ -16,11 +16,12 @@ module.exports = async function (deployer) {
     ** currentEpoch = Math.floor(currentDate/1000) ; 
     ** const expiryDate = currentEpoch + 200000; 
     */
+    await deployer.deploy(PBM, "expiry_uri") ; 
+    pbm = await PBM.deployed(); 
 
     const spot = await Spot.deployed() ; 
     const address = spot.address; 
     // If you are deploying to a public testnet/mainnet , and want to use an existing ERC-20 contract, COMMENT OUT the lines above, and UNCOMMENT the code below
     // const address = 0xDC3326e71D45186F113a2F448984CA0e8D201995 ;     // Polygon XSGD address 
-    await deployer.deploy(PBM, address, expiryDate, "expiry_uri") ;  
-    await pbmAddr.initialise(address, expiryDate, pbmAddr.address);    
+    await pbm.initialise(address, expiryDate, pbmAddr.address);    
 }
