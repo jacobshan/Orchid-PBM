cd73bf9912bc633db6b6182293cd1be3272f0ad6
Mihir <mihirmohan38@gmail.com>
2022-10-11 15:20:04 +0800

    chore: documenting migration 4_airdrop_pbms

--- migrations/4_airdrop_pbms.js
+++ migrations/4_airdrop_pbms.js
@@ -4,25 +4,35 @@ const PBMAddr = artifacts.require("PBMAddressList") ;
 
 module.exports = async function (deployer,network, accounts) {
 
-    // //const spot = await Spot.deployed() ; 
-    // const pbm = await PBM.deployed(); 
-    // //const pbmAddr = await PBMAddr.deployed() ; 
+    // Migration to create and deploy PBM token types
+    // Commented our since this is not required by all. UNCOMMENT and use as necessary
 
-    // currentDate = new Date()
-    // currentEpoch = Math.floor(currentDate/1000) ; 
+    // //const spot = await Spot.deployed() ; // UNCOMMENT  if you are using the ERC20 deployed in the second migration 
+    // const pbm = await PBM.deployed(); 
+
+    // // currentDate = new Date()
+    // // currentEpoch = Math.floor(currentDate/1000) ; 
     // //var targetEpoch = currentEpoch+10000000;  // Expiry is set to 115 days, 17 hours, 46 minutes and 40 seconds
     // var targetEpoch = 1668070453; // november 11 is 1668070453
+
+
+    // // Increasing allowance on deployed ERC20 tokens. UNCOMMENT below if you are using the ERC20 deployed in the second migration
     // //await spot.mint(accounts[0], 100 ) ; 
     // //console.log("minted spot") ; 
+
+    // // creating new PBM token types
     // await pbm.createPBMTokenType("StraitsX", 10000, targetEpoch, accounts[0], "https://gateway.pinata.cloud/ipfs/QmXd8njnZTehRcVSNPvSwLguVUgMB5ySWM3qpRaUaXEU6D" ) ; 
-    // console.log("token type 5$ created") ; 
-    // await new Promise(r => setTimeout(r, 5000));
+    // console.log("PBM Token type 1 created") ; 
+    // //await new Promise(r => setTimeout(r, 5000)); // UNCOMMENT to prevent rpc rate limiting if you are on free version
+
     // await pbm.createPBMTokenType("Grab",10000, targetEpoch, accounts[0], "https://gateway.pinata.cloud/ipfs/QmbGXCVgEHPCkJX81vceVdBH9gnKvja5aT3GWhNuwDbrHR") ;
-    // console.log("token type 10$ created") ; 
-    // await new Promise(r => setTimeout(r, 5000));
-    // // manually increasea allowance
+    // console.log("PBM Token type 2 created") ; 
+    // //await new Promise(r => setTimeout(r, 5000)); // UNCOMMENT to prevent rpc rate limiting if you are on free version
+
+    // // Increasing allowance on deployed ERC20 tokens. UNCOMMENT below if you are using the ERC20 deployed in the second migration
     // //await spot.increaseAllowance(pbm.address, 100) ; 
-    // await new Promise(r => setTimeout(r, 5000));
+    // //await new Promise(r => setTimeout(r, 5000)); // UNCOMMENT to prevent rpc rate limiting if you are on free version
+
     // await pbm.batchMint([0,1], [5,5], accounts[0]) ; 
-    // console.log("pbms minted") ; 
+    // console.log("PBM created and minted") ; 
 }
