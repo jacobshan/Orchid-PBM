e25569ebbff1f6366281be9c13b8b122fb8d8c14
Mihir <mihirmohan38@gmail.com>
2022-10-11 13:03:24 +0800

    chore: fixing comments

--- migrations/2_deploy_spot.js
+++ migrations/2_deploy_spot.js
@@ -2,5 +2,5 @@ const Spot = artifacts.require("Spot") ;
 
 module.exports = async function (deployer) {
     // comment out if you don't need to deploy an ERC-20 token. 
-    deployer.deploy(Spot) ; 
+    //deployer.deploy(Spot) ; 
 }
--- migrations/4_airdrop_pbms.js
+++ migrations/4_airdrop_pbms.js
@@ -4,23 +4,24 @@ const PBMAddr = artifacts.require("PBMAddressList") ;
 
 module.exports = async function (deployer,network, accounts) {
 
-    const spot = await Spot.deployed() ; 
+    //const spot = await Spot.deployed() ; 
     const pbm = await PBM.deployed(); 
-    const pbmAddr = await PBMAddr.deployed() ; 
+    //const pbmAddr = await PBMAddr.deployed() ; 
 
     currentDate = new Date()
     currentEpoch = Math.floor(currentDate/1000) ; 
-    var targetEpoch = currentEpoch+10000000;  // Expiry is set to 115 days, 17 hours, 46 minutes and 40 seconds
-
-    await spot.mint(accounts[0], 100 ) ; 
-    console.log("minted spot") ; 
-    await pbm.createPBMTokenType("StraitsX", 5, targetEpoch, accounts[0], "https://gateway.pinata.cloud/ipfs/QmWZ4mWyvHPoKmnsTQmh76DX9G5T2kMq9KttQH7mp55P6a" ) ; 
+    //var targetEpoch = currentEpoch+10000000;  // Expiry is set to 115 days, 17 hours, 46 minutes and 40 seconds
+    var targetEpoch = 1668070453; // november 11 is 1668070453
+    //await spot.mint(accounts[0], 100 ) ; 
+    //console.log("minted spot") ; 
+    await pbm.createPBMTokenType("StraitsX", 10000, targetEpoch, accounts[0], "https://gateway.pinata.cloud/ipfs/QmXd8njnZTehRcVSNPvSwLguVUgMB5ySWM3qpRaUaXEU6D" ) ; 
     console.log("token type 5$ created") ; 
     await new Promise(r => setTimeout(r, 5000));
-    await pbm.createPBMTokenType("StraitsX",10, targetEpoch, accounts[0], "https://gateway.pinata.cloud/ipfs/QmSyo8gFNhJ6J1t8R1QAWFi3mU8xGQkFKkAoncqAE5EEjV") ;
+    await pbm.createPBMTokenType("Grab",10000, targetEpoch, accounts[0], "https://gateway.pinata.cloud/ipfs/QmbGXCVgEHPCkJX81vceVdBH9gnKvja5aT3GWhNuwDbrHR") ;
     console.log("token type 10$ created") ; 
     await new Promise(r => setTimeout(r, 5000));
-    await spot.increaseAllowance(pbm.address, 100) ; 
+    // manually increasea allowance
+    //await spot.increaseAllowance(pbm.address, 100) ; 
     await new Promise(r => setTimeout(r, 5000));
     await pbm.batchMint([0,1], [5,5], accounts[0]) ; 
     console.log("pbms minted") ; 
