0fa048a59ed7f7a6dd06c28878ea3bb60b80714b
Victor Liew <1174679+alcedo@users.noreply.github.com>
2023-04-25 15:29:26 +0800

    Added test cases and implementation example

--- PBMRC/PBMRC2/contracts/issuer_helper.sol
+++ PBMRC/PBMRC2/contracts/issuer_helper.sol
@@ -1,24 +1,59 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-contract IssuerHelper {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// list of whitelisted wallet addresses that 
-// has granted PBM approval to this smart contract.
-// <CODE HERE>
+interface IPBM {
+    function loadAndPay(address _to, uint256 _amount) external;
+}
 
-// store an address reference to the target PBM
-// <CODE>
+contract IssuerHelper is Ownable {
+    using SafeERC20 for IERC20;
 
-// list of allowedWhitelister to call functions on this smart contract
-// <CODE HERE> 
+    // list of whitelisted wallet addresses that 
+    // has granted PBM approval to this smart contract.
+    mapping(address => bool) public whitelistedWallets;
 
-// this function will call loadAndPay on a target smart contract at an address
-// this function will then call a ERC20 safeTransferFrom to transfer money from a t
-// target wallet address to this smart contract
-// check that the target smart address has implemented loadAndPay
-// <CODE HERE>
+    // store an address reference to the target PBM
+    IPBM public targetPBM;
 
+    // list of allowedWhitelister to call functions on this smart contract
+    mapping(address => bool) public allowedWhitelisters;
 
+    constructor(address _targetPBM) {
+        targetPBM = IPBM(_targetPBM);
+    }
 
+    // this function will call loadAndPay on a target smart contract at an address
+    // this function will then call a ERC20 safeTransferFrom to transfer money from a t
+    // target wallet address to this smart contract
+    // check that the target smart address has implemented loadAndPay
+    function processLoadAndPay(address _erc20Token, address _wallet, uint256 _amount) external onlyWhitelister {
+        require(whitelistedWallets[_wallet], "Wallet is not whitelisted");
+        IERC20(_erc20Token).safeTransferFrom(_wallet, address(this), _amount);
+        targetPBM.loadAndPay(_wallet, _amount);
+    }
+
+    function addWhitelistedWallet(address _wallet) external onlyOwner {
+        whitelistedWallets[_wallet] = true;
+    }
+
+    function removeWhitelistedWallet(address _wallet) external onlyOwner {
+        whitelistedWallets[_wallet] = false;
+    }
+
+    function addWhitelister(address _whitelister) external onlyOwner {
+        allowedWhitelisters[_whitelister] = true;
+    }
+
+    function removeWhitelister(address _whitelister) external onlyOwner {
+        allowedWhitelisters[_whitelister] = false;
+    }
+
+    modifier onlyWhitelister() {
+        require(allowedWhitelisters[msg.sender], "Caller is not an allowed whitelister");
+        _;
+    }
 }
--- PBMRC/PBMRC2/contracts/pbmrc2.sol
+++ PBMRC/PBMRC2/contracts/pbmrc2.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-interface PBMRC2 {
+import "../contracts/PBM.sol";
+
+contract PBMRC2 is PBM {
+    
+
+    // This function load will take ERC20 token from a specified wallet address and move it into 
+    // this smart contract
+    // We will subsequently track how much ERC20 token has been taken for a particular user address
+    // and for which user's tokenId 
+    // <CODE HERE>
+
+    // this function loadAndSafeTransfer will call the function load, and upon success, call safeTransferFrom 
+    // to the target wallet address
+    // <CODE HERE>
 
 }
