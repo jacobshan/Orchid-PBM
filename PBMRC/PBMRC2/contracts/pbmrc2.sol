3ab43ae47134bedc35d122f722d1a35b3466cd32
Victor Liew <1174679+alcedo@users.noreply.github.com>
2023-04-25 15:34:36 +0800

    added implementation for PBMRC2.sol

--- PBMRC/PBMRC2/contracts/pbmrc2.sol
+++ PBMRC/PBMRC2/contracts/pbmrc2.sol
@@ -2,18 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "../contracts/PBM.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract PBMRC2 is PBM {
-    
+    // mapping(address => mapping(uint256 => uint256)) public userTokenBalances;
 
-    // This function load will take ERC20 token from a specified wallet address and move it into 
-    // this smart contract
-    // We will subsequently track how much ERC20 token has been taken for a particular user address
-    // and for which user's tokenId 
-    // <CODE HERE>
+    // function load(address user, uint256 tokenId, uint256 amount) external {
+    //     // Transfer ERC20 tokens from the user's wallet to this smart contract
+    //     IERC20(spotToken).transferFrom(user, address(this), amount);
 
-    // this function loadAndSafeTransfer will call the function load, and upon success, call safeTransferFrom 
-    // to the target wallet address
-    // <CODE HERE>
+    //     // Update the user's balance for the specified tokenId
+    //     userTokenBalances[user][tokenId] += amount;
+    // }
 
+    // function loadAndSafeTransfer(address from, uint256 tokenId, uint256 amount, address to) external {
+    //     // Call the load function to transfer ERC20 tokens and update the balance
+    //     load(from, tokenId, amount);
+
+    //     // Call safeTransferFrom to transfer the loaded tokens to the target wallet address
+    //     // safeTransferFrom should trigger the business logic checks before paying the merchant
+           // it should also keep records of cashback details, and original user. 
+    //     safeTransferFrom(from, to, tokenId, amount, "");
+    //     
+     
+    //     
+    // }
 }
