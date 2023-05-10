21085c7c92713586f58b0373d399c2b2ba98e4d6
Jacob <jacob.shan@fazzfinancial.com>
2023-05-11 03:19:17 +0800

    chore: address review comments

--- PBMRC/PBMRC2/contracts/pbmrc2.sol
+++ PBMRC/PBMRC2/contracts/pbmrc2.sol
@@ -9,23 +9,7 @@ import "./PBM.sol";
 contract PBMRC2 is PBM("") {
     mapping(uint256 => uint256) public PBMDiscounts;
 
-    constructor() {
-        PBMDiscounts[1] = 5; // 5 XSGD discount for Token ID 1
-        PBMDiscounts[2] = 20; // 10 XSGD discount for Token ID 2
-    }
-
-    function getDiscountAmount(
-        address user,
-        uint256 tokenId,
-        uint256 amount
-    ) public view returns (uint256) {
-        require(balanceOf(user, tokenId) > 0, "User does not have the PBM.");
-
-        uint256 discount = PBMDiscounts[tokenId];
-        uint256 discountedAmount = amount - discount;
-
-        return discountedAmount;
-    }
+    constructor() {}
 
     function loadAndSafeTransfer(address _to, uint256 _amount) public {
         // business logic here
